1c571a677cd273acffc8439dcc4ddd95e38c3984
avin1508 <omkumar55800@gmail.com>
2025-09-20 00:06:08 +0530

    pushing first comit

---
+++ src/featuers/auth/screens/SplashScreen.jsx
@@ -0,0 +1,25 @@
+import 
+{ 
+ View, 
+ Text,
+ StyleSheet 
+} from 'react-native'
+import React from 'react'
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>SplashScreen</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+
+    }
+})
+
+export default SplashScreen
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,9 +3,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import AuthStack from "./stacks/AuthStack";
 import TabNavigator from "./TabNavigator";
 
+import SplashScreen from "../featuers/auth/screens/SplashScreen";
+
 const isAuth = false;
+const isLoading = true; // example flag for splash
 
 const AppNavigator = () => {
+  if (isLoading) {
+    return <SplashScreen />;
+  }
+
   return (
     <NavigationContainer>
       {isAuth ? <TabNavigator /> : <AuthStack />}
